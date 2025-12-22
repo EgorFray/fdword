ba2e50fc59d5401512458c6aad0812811302e6a3
EgorFray <fatamorganaa933@gmail.com>
2025-12-22 23:07:43 +0200

    Discovering library etree

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -1,38 +1,44 @@
 package main
 
 import (
-	"archive/zip"
 	"fmt"
-	"io"
-	"log"
-	"os"
+
+	"github.com/beevik/etree"
 )
 
 
 func main() {
-	testdata := "test/test.docx"
-	doc, err := zip.OpenReader(testdata)
-	if err != nil {
-		log.Fatalf("Failed to open doc: %v", err)
+	doc := etree.NewDocument();
+	if err := doc.ReadFromFile("test/styles.xml"); err != nil {
+		panic(err);
 	}
-	defer doc.Close()
 
-	for _, file := range doc.File {
-		fmt.Println(file.Name)
-		if file.Name == "word/styles.xml" {
-			data, err := file.Open()
-			if err != nil {
-				log.Fatal(err)
-			}
+	root := doc.SelectElement("w:styles")
+	fmt.Println("ROOT element: ", root.Tag)
 
-			d, err := io.ReadAll(data)
-			if err != nil {
-				log.Fatal(err)
-			}
+	// testdata := "test/test.docx"
+	// doc, err := zip.OpenReader(testdata)
+	// if err != nil {
+	// 	log.Fatalf("Failed to open doc: %v", err)
+	// }
+	// defer doc.Close()
 
-			fmt.Println(string(d))
-			os.WriteFile("styles.xml", d, 0644)
-		}
-	}
+	// for _, file := range doc.File {
+	// 	fmt.Println(file.Name)
+	// 	if file.Name == "word/styles.xml" {
+	// 		data, err := file.Open()
+	// 		if err != nil {
+	// 			log.Fatal(err)
+	// 		}
+
+	// 		d, err := io.ReadAll(data)
+	// 		if err != nil {
+	// 			log.Fatal(err)
+	// 		}
+
+	// 		fmt.Println(string(d))
+	// 		os.WriteFile("styles.xml", d, 0644)
+	// 	}
+	// }
 
 }
--- backend/go.mod
+++ backend/go.mod
@@ -1,3 +1,7 @@
 module github.com/EgorFray/fdword
 
-go 1.21.0
+go 1.23.0
+
+toolchain go1.24.11
+
+require github.com/beevik/etree v1.6.0 // indirect
