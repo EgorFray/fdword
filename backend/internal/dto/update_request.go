abe5b9d89ef3210449912ee5a2acf4658a5233cf
EgorFray <fatamorganaa933@gmail.com>
2026-02-15 21:36:55 +0200

    Create service and method FormatDoc

---
+++ backend/internal/dto/update_request.go
@@ -0,0 +1,6 @@
+package dto
+
+// That's what I've got from the frontend
+type UpdateRequest struct {
+	LineSpacing *float64 `json:"lineSpacing"`
+}
--- backend/internal/service/format_service.go
+++ backend/internal/service/format_service.go
@@ -2,13 +2,34 @@ package service
 
 import (
 	doc "github.com/EgorFray/fdword/internal/doc"
+	"github.com/EgorFray/fdword/internal/dto"
 )
 
-type FormatService struct {
-	DocModifier doc.DocModifierInterface
-}
+type FormatService struct {}
 
-func NewFormatService(docM doc.DocModifierInterface) *FormatService {
-	return &FormatService{DocModifier: docM}
-}
 
+func (f *FormatService) FormatDoc(fileBytes []byte, req dto.UpdateRequest) ([]byte, error) {
+	// Loading the document
+	file, err := doc.Load(fileBytes)
+	if err != nil {
+		return nil, err
+	}
+
+	// Create modifier
+	modifier := doc.NewDocModifier(file)
+
+	// Check if we have a neccessary field. If yes - call SetLineSpacing method.
+	if req.LineSpacing != nil {
+		if err := modifier.SetLineSpacing(*req.LineSpacing); err != nil {
+			return nil, err
+		}
+	}
+
+	// Save results
+	result, err := doc.Save()
+	if err != nil {
+		return nil, err
+	}
+
+	return result, nil
+}
