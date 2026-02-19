4966396a8f1cd02314f6976dba9d899d4017561c
EgorFray <fatamorganaa933@gmail.com>
2026-02-19 13:03:22 +0200

    Add tailwind and configuration for it

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,12 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Dashboard from "./ui/Dashboard";
 
 function App() {
 	return (
 		<BrowserRouter>
 			<Routes>
-				<Route index element={<Navigate replace to="" />} />
-				<Route path="/dashboard" element="" />
+				<Route index element={<Navigate replace to="/dashboard" />} />
+				<Route path="/dashboard" element={<Dashboard />} />
 			</Routes>
 		</BrowserRouter>
 	);
---
+++ frontend/src/ui/Dashboard.jsx
@@ -0,0 +1,9 @@
+function Dashboard() {
+  return (
+    <div className="flex items-center bg-red-500 p-4 text-center">
+      Testing Tailwind
+    </div>
+  );
+}
+
+export default Dashboard;
