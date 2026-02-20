556ff7b25c888a4ec39c082d119306c41e739ba5
EgorFray <fatamorganaa933@gmail.com>
2026-02-20 21:05:03 +0200

    Add manual page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,17 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-import Dashboard from "./ui/Dashboard";
+import Dashboard from "./pages/Dashboard";
+import Manual from "./pages/Manual";
 
 function App() {
-	return (
-		<BrowserRouter>
-			<Routes>
-				<Route index element={<Navigate replace to="/dashboard" />} />
-				<Route path="/dashboard" element={<Dashboard />} />
-			</Routes>
-		</BrowserRouter>
-	);
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Navigate replace to="/dashboard" />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/manual" element={<Manual />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ frontend/src/pages/Manual.jsx
@@ -0,0 +1,5 @@
+function Manual() {
+  return <div>this is page with instruction on how to use this app</div>;
+}
+
+export default Manual;
