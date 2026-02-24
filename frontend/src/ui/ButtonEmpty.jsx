ccc2e0121025433ce9c4eebfa0ff6fd02e044911
EgorFray <fatamorganaa933@gmail.com>
2026-02-24 22:08:35 +0200

    ModifyForm styling

---
+++ frontend/src/ui/ButtonEmpty.jsx
@@ -0,0 +1,9 @@
+function ButtonEmpty({ children }) {
+  return (
+    <button className="text-md cursor-pointer self-center rounded-full px-4 py-2 tracking-wide text-blue-950 shadow-[0_4px_6px_-1px_rgba(0,0,0,0.1)] transition-colors duration-300 hover:bg-gray-300">
+      {children}
+    </button>
+  );
+}
+
+export default ButtonEmpty;
--- frontend/src/ui/ModifyForm.jsx
+++ frontend/src/ui/ModifyForm.jsx
@@ -1,18 +1,47 @@
+import Button from "./Button";
+import ButtonEmpty from "./ButtonEmpty";
+
 function ModifyForm() {
   return (
-    <form className="max-w-160 border border-blue-950">
-      <h2>Select what to change</h2>
+    <div className="flex justify-center">
+      <form className="max-w-160 items-start justify-items-start rounded-xl border border-blue-950/40 p-6">
+        <h2 className="mb-6 ml-4 text-2xl font-bold">Select what to change</h2>
 
-      <div>
-        <label>Line spacing</label>
-        <input type="text" />
-      </div>
+        <div className="mb-6 grid grid-cols-[200px_1fr_1fr] items-center justify-items-start">
+          <label
+            htmlFor="lineSpace"
+            className="ml-4 text-lg font-semibold text-blue-950/80"
+          >
+            Line spacing
+          </label>
+          <input
+            id="lineSpace"
+            type="text"
+            placeholder="For example 1 or 1.15"
+            className="rounded-lg bg-white p-1 pl-2"
+          />
+        </div>
 
-      <div>
-        <label>File</label>
-        <input type="file" />
-      </div>
-    </form>
+        <div className="mb-6 grid grid-cols-[200px_1fr_1fr] items-center justify-items-start">
+          <label
+            htmlFor="file"
+            className="ml-4 text-lg font-semibold text-blue-950/80"
+          >
+            File
+          </label>
+          <input
+            type="file"
+            id="file"
+            className="file:cursor-pointer file:self-center file:rounded-full file:bg-blue-600 file:px-4 file:py-2 file:tracking-wide file:text-blue-50 file:shadow-[0_4px_6px_-1px_rgba(0,0,0,0.1)] file:transition-colors file:duration-300"
+          />
+        </div>
+
+        <div className="mr-4 flex w-full justify-end gap-2">
+          <ButtonEmpty>Cancel</ButtonEmpty>
+          <Button>Submit</Button>
+        </div>
+      </form>
+    </div>
   );
 }
 
