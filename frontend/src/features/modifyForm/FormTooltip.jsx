74f8b6f3077f3d44e9ee5401b8ad39c4052811f5
EgorFray <fatamorganaa933@gmail.com>
2026-03-15 17:59:37 +0200

    Add oppacity animation for tooltip

--- frontend/src/features/modifyForm/FormTooltip.jsx
+++ frontend/src/features/modifyForm/FormTooltip.jsx
@@ -1,31 +1,53 @@
 import { BsQuestionCircle } from "react-icons/bs";
 import { useState } from "react";
-import { useFloating, offset } from "@floating-ui/react";
+import {
+  useFloating,
+  useHover,
+  offset,
+  useInteractions,
+  useTransitionStyles,
+} from "@floating-ui/react";
 import Tooltip from "../../ui/Tooltip";
 
 function FormTooltip({ video, tooltip }) {
-  const { refs, floatingStyles } = useFloating({
+  const [isOpenTooltip, setIsOpenTooltip] = useState(false);
+  const { refs, floatingStyles, context } = useFloating({
     placement: "right",
     middleware: [offset(10)],
+    open: isOpenTooltip,
+    onOpenChange: setIsOpenTooltip,
   });
-  const [isOpenTooltip, setIsOpenTooltip] = useState(false);
+  const { styles: transitionStyles } = useTransitionStyles(context, {
+    duration: {
+      open: 300,
+      close: 400,
+    },
+  });
+
+  const hover = useHover(context, {
+    delay: 150,
+  });
+
+  const { getReferenceProps, getFloatingProps } = useInteractions([hover]);
 
   return (
     <div
       onClick={() => setIsOpenTooltip((isOpen) => !isOpen)}
-      onMouseEnter={() => setIsOpenTooltip(true)}
-      onMouseLeave={() => setIsOpenTooltip(false)}
+      // onMouseEnter={() => setIsOpenTooltip(true)}
+      // onMouseLeave={() => setIsOpenTooltip(false)}
     >
       <BsQuestionCircle
         ref={refs.setReference}
+        {...getReferenceProps()}
         className="cursor-pointer text-gray-400"
       />
       {isOpenTooltip && (
         <Tooltip
           ref={refs.setFloating}
-          style={floatingStyles}
+          style={{ ...floatingStyles, ...transitionStyles }}
           video={video}
           tooltip={tooltip}
+          {...getFloatingProps()}
         />
       )}
     </div>
