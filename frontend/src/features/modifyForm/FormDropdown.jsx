fab468fe85ccc35c853d725799343f577801d2c5
EgorFray <fatamorganaa933@gmail.com>
2026-03-16 16:01:12 +0200

    Add smooth scrolling to open dropdown rows

--- frontend/src/features/modifyForm/FormDropdown.jsx
+++ frontend/src/features/modifyForm/FormDropdown.jsx
@@ -1,9 +1,21 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { motion, AnimatePresence } from "motion/react";
 import { BsChevronDown, BsChevronDoubleDown } from "react-icons/bs";
 
 function FormDropdown({ title, children }) {
   const [isDropOpen, setIsDropOpen] = useState(false);
+  const rowRef = useRef(null);
+
+  useEffect(() => {
+    if (isDropOpen && rowRef.current) {
+      setTimeout(() => {
+        rowRef.current.scrollIntoView({
+          behavior: "smooth",
+          block: "center",
+        });
+      }, 50);
+    }
+  }, [isDropOpen]);
 
   return (
     <>
@@ -23,30 +35,21 @@ function FormDropdown({ title, children }) {
 
       {
         <AnimatePresence>
-          isDropOpen && (
-          <motion.div
-            initial={{ opacity: 0, height: 0 }}
-            animate={
-              isDropOpen
-                ? { opacity: 1, height: "auto" }
-                : { opacity: 0, height: 0 }
-            }
-            transition={
-              isDropOpen
-                ? {
-                    height: { duration: 0.25 },
-                    opacity: { delay: 0.2, duration: 0.25 },
-                  }
-                : {
-                    height: { delay: 0.25, duration: 0.25 },
-                    opacity: { duration: 0.25 },
-                  }
-            }
-            style={{ overflow: "hidden" }}
-          >
-            {children}
-          </motion.div>
-          )
+          {isDropOpen && (
+            <motion.div
+              initial={{ opacity: 0, height: 0 }}
+              animate={{ opacity: 1, height: "auto" }}
+              transition={{
+                height: { duration: 0.25 },
+                opacity: { delay: 0.2, duration: 0.25 },
+              }}
+              exit={{ opacity: 0, height: 0 }}
+              style={{ overflow: "hidden" }}
+              ref={rowRef}
+            >
+              {children}
+            </motion.div>
+          )}
         </AnimatePresence>
       }
     </>
