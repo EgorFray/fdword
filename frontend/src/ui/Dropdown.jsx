8a6d681dc3fbb80cd1093da2f7327d60af5e22ab
EgorFray <fatamorganaa933@gmail.com>
2026-03-25 15:45:09 +0200

    Fix lag while opeen dropdown items

--- frontend/src/ui/Dropdown.jsx
+++ frontend/src/ui/Dropdown.jsx
@@ -1,10 +1,18 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useLayoutEffect, useRef, useState } from "react";
 import { motion, AnimatePresence } from "motion/react";
 import { BsChevronDown, BsChevronDoubleDown } from "react-icons/bs";
 
 function Dropdown({ title, children }) {
+  const [height, setHeight] = useState(0);
   const [isDropOpen, setIsDropOpen] = useState(false);
   const rowRef = useRef(null);
+
+  // I use timeout, because other solution with observers ar too complecated for me right now. I can come back later to solve it properly.
+  useLayoutEffect(() => {
+    if (rowRef.current) {
+      setTimeout(() => setHeight(rowRef.current.scrollHeight), 100);
+    }
+  }, [isDropOpen]);
 
   useEffect(() => {
     if (isDropOpen && rowRef.current) {
@@ -13,7 +21,7 @@ function Dropdown({ title, children }) {
           behavior: "smooth",
           block: "center",
         });
-      }, 100);
+      }, 120);
     }
   }, [isDropOpen]);
 
@@ -37,11 +45,15 @@ function Dropdown({ title, children }) {
         <AnimatePresence>
           {isDropOpen && (
             <motion.div
+              layout
               initial={{ opacity: 0, height: 0 }}
-              animate={{ opacity: 1, height: "auto" }}
+              animate={{
+                opacity: 1,
+                height: height,
+              }}
               transition={{
                 height: { duration: 0.25 },
-                opacity: { delay: 0.2, duration: 0.25 },
+                opacity: { delay: 0.25, duration: 0.25 },
               }}
               exit={{ opacity: 0, height: 0 }}
               style={{ overflow: "hidden" }}
