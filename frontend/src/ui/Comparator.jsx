d51fddd9cb8c5be8893bc3404afd80ffffa97831
EgorFray <fatamorganaa933@gmail.com>
2026-03-26 12:40:42 +0200

    Add nw images to Comparator on the dashboard pag

--- frontend/src/ui/Comparator.jsx
+++ frontend/src/ui/Comparator.jsx
@@ -1,8 +1,11 @@
 function Comparator() {
   return (
     <div className="grid grid-cols-2 rounded-xl shadow-[0_4px_6px_-1px_rgba(0,0,0,0.1)]">
-      <img src="/before.png" className="h-auto rounded-tl-xl rounded-bl-xl" />
-      <img src="/after.png" className="h-auto rounded-tr-xl rounded-br-xl" />
+      <img
+        src="/before.png"
+        className="rounded-tl-xl rounded-bl-xl border-r border-blue-950/20"
+      />
+      <img src="/after.png" className="rounded-tr-xl rounded-br-xl" />
     </div>
   );
 }
--- frontend/src/ui/DashboardHeading.jsx
+++ frontend/src/ui/DashboardHeading.jsx
@@ -10,7 +10,7 @@ function DashboardHeading({ formRef }) {
   return (
     <div className="flex flex-col gap-5">
       <p className="text-3xl font-bold">
-        Transform your document into <br /> well-formated document
+        Transform your word file into <br /> well-formated document
       </p>
       <p className="text-xl font-medium text-blue-950/80">
         Just add your document, choose what <br />
