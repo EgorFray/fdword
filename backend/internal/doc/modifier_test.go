bd827deb9a806a7093836f75a68f5a27db67be86
EgorFray <fatamorganaa933@gmail.com>
2026-04-01 15:26:26 +0300

    test of setFontSize

--- backend/internal/doc/modifier_test.go
+++ backend/internal/doc/modifier_test.go
@@ -24,3 +24,20 @@ func TestSetLineSpacing(t *testing.T) {
 	val := spacing.SelectAttrValue("w:line", "")
 	assert.Equal(t, "480", val)
 }
+
+func TestSetFontSize(t *testing.T) {
+	doc := LoadTestDoc(t, "../testdata/styles.xml", "../testdata/document.xml")
+
+	modifier := NewDocModifier(doc)
+
+	err := modifier.SetFontSize(16)
+	if err != nil {
+		t.Fatal(err)
+	}
+	
+	fontSize := doc.Styles.Root().FindElement("w:docDefaults/w:rPrDefault/w:rPr/w:sz")
+
+	val := fontSize.SelectAttrValue("w:val", "")
+	// assert with 32 because in word font size is 2 times bigger than input value
+	assert.Equal(t, "32", val)
+}
