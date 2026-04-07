9e1050c7b8e8c761f863503582d1c3329de2e8d7
EgorFray <fatamorganaa933@gmail.com>
2026-04-07 15:42:45 +0300

    Restyling manual link

--- frontend/src/ui/Header.jsx
+++ frontend/src/ui/Header.jsx
@@ -3,9 +3,12 @@ import Logo from "./Logo";
 
 function Header() {
   return (
-    <header className="mt-4 flex items-center justify-between px-4 text-xl font-semibold">
+    <header className="mt-4 flex items-center justify-between px-4">
       <Logo />
-      <NavLink to="/manual" className="transition-colors hover:text-blue-800">
+      <NavLink
+        to="/manual"
+        className="cursor-pointer self-center rounded-full bg-blue-600 px-4 py-2 text-sm tracking-wide text-blue-50 shadow-[0_4px_6px_-1px_rgba(0,0,0,0.1)] transition-colors duration-300 hover:bg-blue-500 md:text-base"
+      >
         Manual
       </NavLink>
     </header>
