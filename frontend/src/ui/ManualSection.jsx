d094df745b5f84b1f03fd2e635194278a1db0545
EgorFray <fatamorganaa933@gmail.com>
2026-04-08 09:42:36 +0300

    Change width size of images in manual page

--- frontend/src/ui/ManualSection.jsx
+++ frontend/src/ui/ManualSection.jsx
@@ -1,17 +1,15 @@
 function ManualSection({ argName, image, children }) {
   return (
     <section className="mx-4 mt-2 mb-2 flex flex-col gap-3 md:mt-4 md:mb-6 md:grid md:grid-cols-[400px_1fr] md:gap-x-6">
-      {/* Image 1 (before) */}
-      <div className="col-start-1 row-start-1">
+      <div className="flex items-center justify-center">
         <img
           src={image}
           alt={`Example of ${argName} before changes`}
-          className="w-full rounded-xl"
+          className="w-4/6 rounded-xl md:w-full"
         />
       </div>
 
-      {/* Description */}
-      <div className="col-start-2 row-span-2 ml-1 text-start text-base font-medium text-blue-950/80 md:ml-0 md:text-lg">
+      <div className="ml-1 text-start text-base font-medium text-blue-950/80 md:ml-0 md:text-lg">
         {children}
       </div>
     </section>
