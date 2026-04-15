88e24f61ed5abbb70df1cbdbb2d5f9a227387363
EgorFray <fatamorganaa933@gmail.com>
2026-04-15 14:09:15 +0300

    Add server ip to origins in cors.go

--- backend/config/cors.go
+++ backend/config/cors.go
@@ -4,7 +4,7 @@ import "github.com/gin-contrib/cors"
 
 func CorsConfig() cors.Config {
 	return cors.Config{
-		AllowOrigins:     []string{"http://localhost:5173"},
+		AllowOrigins:     []string{"http://localhost:5173", "http://178.104.113.14:3000"},
     AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
     AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization", "Access-Control-Allow-Origin"},
     ExposeHeaders:    []string{"Content-Length"},
