3f955713cdf2c742807468365a75bf86d4cc3005
EgorFray <fatamorganaa933@gmail.com>
2026-04-22 14:18:06 +0300

    Update footer

--- frontend/src/ui/Footer.jsx
+++ frontend/src/ui/Footer.jsx
@@ -1,6 +1,9 @@
 function Footer() {
+  const curYear = new Date();
   return (
-    <div className="my-4 flex items-center justify-center">This is footer</div>
+    <div className="my-4 flex items-center justify-center">
+      Docxfixer {curYear.getFullYear()}. All rights reserved.
+    </div>
   );
 }
 
