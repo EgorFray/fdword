e31b32e626a1c0dedd1609ae420d8c1d0e177f77
EgorFray <fatamorganaa933@gmail.com>
2026-04-29 13:02:35 +0300

    Update helper function to create normal style if there is no one

--- backend/internal/doc/modifier_helpers.go
+++ backend/internal/doc/modifier_helpers.go
@@ -7,11 +7,20 @@ import (
 // In word we should not only change docDefaults in Styles.xml but also normal style in Style.xml
 func (d *DocModifier) getNormalStyle() *etree.Element {
 	root := d.doc.Styles.Root()
-	
+
 	for _, style := range root.FindElements("//w:style") {
 		if style.SelectAttrValue("w:styleId", "") == "Normal" {
 			return style
 		}
+		}
+		style := root.CreateElement("w:style")
+		style.CreateAttr("w:type", "paragraph")
+		style.CreateAttr("w:default", "1")
+		style.CreateAttr("w:styleId", "Normal")
+
+		name := style.CreateElement("w:name")
+		name.CreateAttr("w:val", "Normal")
+
+		return nil
 	}
-	return nil
-}
+	
