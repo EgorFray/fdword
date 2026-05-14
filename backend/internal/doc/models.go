8ac24c7ca63089a83c57cbb5ee5b47d56b2a04c1
EgorFray <fatamorganaa933@gmail.com>
2026-05-14 12:52:57 +0300

    Add ListRef struct for getting NumId and Ilvl from Documemt.xml

--- backend/internal/doc/models.go
+++ backend/internal/doc/models.go
@@ -2,9 +2,16 @@ package doc
 
 import "github.com/beevik/etree"
 
+// Main model of neccessary files from word doc
 type WDoc struct {
 	Styles *etree.Document
 	Document *etree.Document
 	Numbering *etree.Document
 	files map[string][]byte
+}
+
+// Helper model to get neccessary fields of styling ListParagraphs
+type ListRef struct {
+	NumId string
+  Ilvl string
 }
