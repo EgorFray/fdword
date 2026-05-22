fcf324275bbfcdf507063dbb0b31d6fc6c26850d
EgorFray <fatamorganaa933@gmail.com>
2026-05-22 16:14:03 +0300

    Finish template-card styling

--- frontend/src/ui/TemplateCard.jsx
+++ frontend/src/ui/TemplateCard.jsx
@@ -1,15 +1,17 @@
 function TemplateCard({ headingCount }) {
   return (
-    <div className="h-50 w-40 cursor-pointer rounded-xl bg-white md:h-60 md:w-50">
-      <div className="mt-5">
-        {Array.from({ length: headingCount }, (_, index) => (
-          <p key={index}> Heading {index + 1}</p>
-        ))}
-
-        <p className="mt-5 p-4 text-left indent-5">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+    <div className="flex h-55 w-40 cursor-pointer flex-col rounded-xl bg-white p-4 shadow-[0_4px_6px_-1px_rgba(0,0,0,0.1)] hover:ring-2 hover:ring-blue-600 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-600 focus-visible:ring-offset-2 md:h-60 md:w-53.5">
+      {Array.from({ length: headingCount }, (_, index) => (
+        <p key={index} className="text-xs md:text-sm">
+          {" "}
+          Heading {index + 1}
         </p>
-      </div>
+      ))}
+
+      <p className="mt-auto text-justify indent-5 text-xs md:text-sm">
+        Lorem ipsum dolor sit amet, consectetur adipi elit. Asperiores ad
+        laudan, atque culpa amet minima eum
+      </p>
     </div>
   );
 }
