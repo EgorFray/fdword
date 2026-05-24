665be44711e9178da36a413336f7ba81a852f34b
EgorFray <fatamorganaa933@gmail.com>
2026-05-24 19:55:32 +0300

    Add titles to templates list

--- frontend/src/services/templatesData.js
+++ frontend/src/services/templatesData.js
@@ -1,18 +1,22 @@
 export const templates = [
   {
     id: "one-heading",
+    title: "First paragraph settings",
     headingCount: 1,
   },
   {
     id: "two-headings",
+    title: "Second paragraph settings",
     headingCount: 2,
   },
   {
     id: "three-headings",
+    title: "Third paragraph settings",
     headingCount: 3,
   },
   {
     id: "four-headings",
+    title: "Forth paragraph settings",
     headingCount: 4,
   },
 ];
--- frontend/src/ui/TemplateCard.jsx
+++ frontend/src/ui/TemplateCard.jsx
@@ -1,9 +1,8 @@
 function TemplateCard({ headingCount }) {
   return (
-    <div className="flex h-55 w-37.5 cursor-pointer flex-col rounded-xl bg-white p-4 shadow-[0_4px_6px_-1px_rgba(0,0,0,0.1)] hover:ring-2 hover:ring-blue-600 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-600 focus-visible:ring-offset-2 md:h-60 md:w-53.5">
+    <button className="flex h-55 w-37.5 cursor-pointer flex-col rounded-xl bg-white p-4 shadow-[0_4px_6px_-1px_rgba(0,0,0,0.1)] hover:ring-2 hover:ring-blue-600 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-600 focus-visible:ring-offset-2 md:h-60 md:w-53.5">
       {Array.from({ length: headingCount }, (_, index) => (
         <p key={index} className="text-xs md:text-sm">
-          {" "}
           Heading {index + 1}
         </p>
       ))}
@@ -12,7 +11,7 @@ function TemplateCard({ headingCount }) {
         Lorem ipsum dolor sit amet, consectetur adipi elit. Asperiores ad
         laudan, atque culpa amet minima eum
       </p>
-    </div>
+    </button>
   );
 }
 
