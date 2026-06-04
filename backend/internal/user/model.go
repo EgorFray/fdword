4b88f33e82e0594e9323efb3c6ba33975be8952e
EgorFray <fatamorganaa933@gmail.com>
2026-06-04 15:15:09 +0300

    Add Create user function to repository

--- backend/internal/user/model.go
+++ backend/internal/user/model.go
@@ -6,7 +6,7 @@ type User struct {
 	ID int64
 	GoogleID string
 	Email string
-	Name string
-	AvatarURL string
+	Name *string
+	AvatarURL *string
 	CreatedAt time.Time
 }
--- backend/internal/user/repository.go
+++ backend/internal/user/repository.go
@@ -38,3 +38,31 @@ func (r *UserRepository) GetUserByGoogleId(ctx context.Context, googleId string)
 
 	return &user, nil
 }
+
+func (r *UserRepository) CreateUser(ctx context.Context, user User) (*User, error) {
+	query := "INSERT INTO users (google_id, email, name, avatar_url) VALUES ($1, $2, $3, $4) RETURNING id, google_id, email, name, avatar_url, created_at"
+
+	var createdUser User
+
+	err := r.db.QueryRowContext(
+		ctx, 
+		query, 
+		user.GoogleID, 
+		user.Email, 
+		user.Name, 
+		user.AvatarURL,
+		).Scan(
+			&createdUser.ID, 
+			&createdUser.GoogleID, 
+			&createdUser.Email, 
+			&createdUser.Name, 
+			&createdUser.AvatarURL, 
+			&createdUser.CreatedAt,
+		)
+	
+		if err != nil {
+			return nil, err
+		}
+
+		return &createdUser, nil
+}
