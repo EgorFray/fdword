ac968ec6e6ae8a1455adf6197bdf6845c29d7b3d
EgorFray <fatamorganaa933@gmail.com>
2026-06-10 10:40:01 +0300

    Add google callback function

--- backend/internal/auth/handler.go
+++ backend/internal/auth/handler.go
@@ -1,6 +1,7 @@
 package auth
 
 import (
+	"encoding/json"
 	"net/http"
 
 	"github.com/EgorFray/fdword/config"
@@ -40,4 +41,61 @@ func (h *AuthHandler) GoogleLogin(c *gin.Context) {
 	url := h.googleOAuthConfig.AuthCodeURL(state)
 
 	c.Redirect(http.StatusTemporaryRedirect, url)
+}
+
+func (h *AuthHandler) GoogleCallback(c *gin.Context) {
+	ctx := c.Request.Context()
+	state := c.Query("state")
+	code := c.Query("code")
+
+	if state != "temporary-state" {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid state"})
+		return
+	}
+
+	if code == "" {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "code is required"})
+		return
+	}
+
+	token, err := h.googleOAuthConfig.Exchange(ctx, code)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "failed to exchange code for token"})
+		return
+	}
+
+	client := h.googleOAuthConfig.Client(ctx, token)
+
+	resp, err := client.Get("https://openidconnect.googleapis.com/v1/userinfo")
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "failed to get user info from google"})
+		return
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "failed to decode google user info"})
+	}
+
+	var googleUserInfo GoogleUserInfo
+
+	if err := json.NewDecoder(resp.Body).Decode(&googleUserInfo); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "failed to decode google user info"})
+		return
+	}
+
+	googleUserData := user.GoogleUser{
+		GoogleID: googleUserInfo.Sub,
+		Email: googleUserInfo.Email,
+		Name: googleUserInfo.Name,
+		AvatarURL: googleUserInfo.Picture,
+	}
+
+	appUser, err := h.userService.GetOrCreateUser(ctx, googleUserData)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "failed to get or create user"})
+		return
+	}
+
+	c.JSON(http.StatusOK, appUser)
 }
