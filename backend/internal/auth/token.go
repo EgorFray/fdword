53d7b646c1f5bf136c564e8a657b3c99146b2b3e
EgorFray <fatamorganaa933@gmail.com>
2026-06-11 19:31:38 +0300

    Add generate JWT method

---
+++ backend/internal/auth/token.go
@@ -0,0 +1,27 @@
+package auth
+
+import (
+	"os"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+type Claims struct {
+	UserID int64 `json:"user_id"`
+	jwt.RegisteredClaims
+}
+
+func GenerateJWT(userId int64) (string, error) {
+	claims := Claims{
+		UserID: userId,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(24 * time.Hour)),
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	return token.SignedString([]byte(os.Getenv("JWT_SECRET")))
+}
