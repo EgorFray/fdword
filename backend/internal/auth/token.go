c1c8bc9180c079e7f0427f4f3288f8a484630716
EgorFray <fatamorganaa933@gmail.com>
2026-06-12 16:06:50 +0300

    Add ParseJWT function

--- backend/internal/auth/token.go
+++ backend/internal/auth/token.go
@@ -25,3 +25,20 @@ func GenerateJWT(userId int64) (string, error) {
 
 	return token.SignedString([]byte(os.Getenv("JWT_SECRET")))
 }
+
+func ParseJWT(tokenString string, secret string) (*Claims, error) {
+	token, err := jwt.ParseWithClaims(tokenString, &Claims{}, func(token *jwt.Token) (interface{}, error) {
+		return []byte(secret), nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	claims, ok := token.Claims.(*Claims)
+	if !ok || !token.Valid {
+		return nil, jwt.ErrTokenInvalidClaims
+	}
+
+	return claims, nil
+}
