b9cb97fbb00b04e7b2cd6fd20c0132caf0f19605
EgorFray <fatamorganaa933@gmail.com>
2026-06-15 16:09:27 +0300

    Add GetDocumentById method

---
+++ backend/internal/document/repository.go
@@ -0,0 +1,42 @@
+package document
+
+import (
+	"context"
+	"database/sql"
+)
+
+type DocumentRepository struct {
+	db *sql.DB
+}
+
+func NewDocumentRepository(db *sql.DB) *DocumentRepository {
+	return &DocumentRepository{db: db}
+}
+
+// Return specific document by docId for downloading it.
+func (r *DocumentRepository) GetDocumentById(ctx context.Context, docId string) (*Document, error) {
+	query := `SELECT id, user_id, original_file_name, formated_file_name, original_file_path, formated_file_path, options_json, created_at FROM documents WHERE id = $1` 
+
+	var document Document
+
+	err := r.db.QueryRowContext(ctx, query, docId).Scan(
+		&document.ID, 
+		&document.UserID,
+		&document.OriginalFileName,
+		&document.FormatedFileName,
+		&document.OriginalFilePath,
+		&document.FormatedFilePath,
+		&document.OptionsJSON,
+		&document.CreatedAt,
+	)
+
+		if err == sql.ErrNoRows {
+		return nil, nil
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &document, nil
+}
